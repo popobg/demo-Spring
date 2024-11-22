2b093122c78af42719d4fa137a96eff1faad8f0e
popobg <pauline.bouyssou1@gmail.com>
2024-11-22 12:49:48 +0100

    Add javadoc

--- src/main/java/fr/diginamic/hello/controllers/HelloControleur.java
+++ src/main/java/fr/diginamic/hello/controllers/HelloControleur.java
@@ -1,13 +1,21 @@
-package fr.diginamic.hello.controleurs;
+package fr.diginamic.hello.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Classe Controller gérant les salutations
+ */
 @RestController
 // mapping de l'url qui permettra à l'API de joindre ce contrôleur
 @RequestMapping("/hello")
 public class HelloControleur {
+
+    /**
+     * Méthode de salutation
+     * @return String hello, salutation informelle et joyeuse
+     */
     @GetMapping
     public String sayHello() {
         return "Hello !";
